0bdaf3571bd063cbfc6ee8c7b4d24e85b130cd17
Muhammed Apak <bufman25@gmail.com>
2022-09-19 22:02:14 +0300

    Last Commit to Project

--- app.js
+++ app.js
@@ -2,60 +2,103 @@ let input = document.getElementById("task")
 let formTodo = document.getElementById("form")
 let ulList = document.getElementById("list")
 
-
-formTodo.addEventListener("submit", addTodo)
+eventListeners()
+function eventListeners (){ // bütün event listener'lar bu fonksiyonun içinde bulunacak.
+   formTodo.addEventListener("submit", addTodo) 
+   ulList.addEventListener("click",deleteTodoFromUI )
+   document.addEventListener("DOMContentLoaded", loadTodoFromStorage)
+}
 
 function addTodo (e){
-    newTodo = input.value;
+    const newTodo = input.value.trim();
     if (newTodo === ""){
-        // uyarı mesajı
-        alert("lütfen boş girmeyin")
+        alertFromUI("danger","Lütfen bir todo giriniz")
     }else {
-        // eklendi mesajı
-        
-        addNewTodo();
-    }
+        addNewTodo(newTodo);
+        addTodoToStorage(newTodo);
+        if(ulList.childElementCount <2) {
+            alertFromUI("primary", "Bir todo eklendi")
+        }else{
+            alertFromUI("primary", "Bir todo daha eklendi")
+        }
+        clearInput()
+    };
     
-    console.log(newTodo);
+    
     e.preventDefault();
 }
 
+function clearInput (){
+    input.value = "";
+}
 
-function addNewTodo () {
-    
+function addNewTodo (todo) {
     let todoLi = document.createElement("li");
-    todoLi.textContent = input.value;
+    todoLi.textContent = todo;
     let iconX = document.createElement("i");
     iconX.classList = `fa-solid fa-x`;
-    todoLi.appendChild(iconX)
-
-    ulList.appendChild(todoLi)
-    addTodosToStorage(todoLi)
-
+    todoLi.appendChild(iconX);
+    ulList.appendChild(todoLi);
+   
 }
 
-function removeTodosFromUI (e) {
-    console.log()
-    // if(e.target.className === `fa-solid fa-x`){
-    //     e.target.parentElement.innerHtml.remove()
-    // }
+
+
+function alertFromUI (type,message) {
+    let alertItem = document.createElement("div");
+    alertItem.classList = `alert alert-${type}`;
+    alertItem.style.position="fixed";
+    alertItem.style.marginTop="1rem";
+    alertItem.textContent = `${message}`;
+    ulList.previousElementSibling.appendChild(alertItem);
+
+    setTimeout(() => {
+        alertItem.remove();
+    }, 1000);
+    
 }
-removeTodosFromUI()
 
-// function addTodosToStorage (todo){
-//     let todos = [todo];
+function addTodoToStorage(newTodo) {
+    let todos = getTodosFromStorage();
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos))
     
-//    console.log(todos)
-// }
-
-// addTodosToStorage()
+}
 
 
-// function getTodoFromStorage () {
-//     let todos = ulList.forEach(liItem => {
-//         console.log(liItem)
-//     });
-    
-// }
+function getTodosFromStorage() { 
+    let todos;
+    if(localStorage.getItem("todos") === null){
+        todos = []
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    return todos;
+}
 
-// getTodoFromStorage()
+function deleteTodoFromUI (e){
+    if(e.target.className === "fa-solid fa-x"){
+        e.target.parentElement.remove();
+        deleteTodoFromStorage(e.target.parentElement.textContent)
+        alertFromUI("warning","Bir todo silindi.")
+    };
+   
+}
+
+function loadTodoFromStorage () {
+    let todos = getTodosFromStorage();
+    todos.forEach(todo => {
+        addNewTodo(todo)
+    });
+}
+
+function deleteTodoFromStorage (deleteTodo) {
+    let todos = getTodosFromStorage();
+    todos.forEach(function (todo,index){
+       if(todo === deleteTodo){
+        todos.splice(index,1)
+       } 
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
